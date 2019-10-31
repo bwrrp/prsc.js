53214100ce23205dd1c1543bea097a3e56626088
Stef Busking <stef.busking@gmail.com>
2019-10-31 11:11:37 +0100

    Extract library from xspattern repo

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
+
+const { main: MAIN_DEST_FILE, module: MODULE_DEST_FILE } = require('./package.json');
+
+export default {
+	input: 'lib/parser-combinators.js',
+	output: [
+		{
+			name: 'prsc',
+			file: MAIN_DEST_FILE,
+			format: 'umd',
+			exports: 'named',
+			sourcemap: true
+		},
+		{ file: MODULE_DEST_FILE, format: 'es', sourcemap: true }
+	],
+	plugins: [
+		sourcemaps(),
+		terser()
+	]
+};
